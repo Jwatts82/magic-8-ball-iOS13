7f5722f40db545f9aac9803ce6a8315d1e5451fd
jwatts82 <wattscodes@gmail.com>
2021-05-22 18:45:34 -0700

    update image view

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -8,15 +8,20 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
-    @IBOutlet weak var imageView: UIImageView!
-    
-    @IBOutlet weak var askButtonPressed: UIButton!
-    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+   
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+        imageView.image = ballArray[2]
+        
+    }
+   
+    
 }
 
+//imageView.image = ballArray[2] askBUttonPressed, imageView
